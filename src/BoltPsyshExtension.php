027477b207bb3a159682a6fd9a0d37ba11fa4b0f
Kenny1911 <o-muzyka@mail.ru>
2019-11-15 17:51:32 +0400

    feat: Init extension

---
+++ src/BoltPsyshExtension.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bolt\Extension\Kenny1911\BoltPsysh;
+
+use Bolt\Extension\SimpleExtension;
+
+/**
+ * ExtensionName extension class.
+ *
+ * @author Your Name <you@example.com>
+ */
+class BoltPsyshExtension extends SimpleExtension
+{
+}
